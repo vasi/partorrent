6f371fb115b012b03da85e25a30b93fc8cb7077d
Dave Vasilevsky <dave@vasilevsky.ca>
2012-06-21 20:30:32 -0400

    Add fastresume

--- simple_client.py
+++ simple_client.py
@@ -8,14 +8,20 @@ import libtorrent as lt
 import time
 import sys
 
+resume_file = '.fastresume'
 ses = lt.session()
 ses.listen_on(6881, 6891)
 
 info = lt.torrent_info(sys.argv[1])
-h = ses.add_torrent({'ti': info, 'save_path': './'})
+params = {'ti': info, 'save_path': './'}
+try:
+	params['resume_data'] = open(resume_file, 'rb').read()
+except:
+	pass
+h = ses.add_torrent(params)
 print 'starting', h.name()
 
-while (not h.is_seed()):
+while (not h.is_finished()):
 	s = h.status()
 
 	state_str = ['queued', 'checking', 'downloading metadata', \
@@ -27,5 +33,6 @@ while (not h.is_seed()):
 
 	time.sleep(1)
 
+open(resume_file, 'wb').write(lt.bencode(h.write_resume_data()))
 print h.name(), 'complete'
 
